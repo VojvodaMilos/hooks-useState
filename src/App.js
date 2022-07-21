e025459805440958a2842b7b8ce8fab933edbc1a
Milos1988 <vojvoda1988>
2022-07-21 11:06:15 +0200

    pocetak na novoj grani

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  return (
-    <div className="container text-center">
-      <h1>{name}</h1>
-      <input
-        type="text"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-    </div>
-  );
+  return <div className="container text-center"></div>;
 }
 
 export default App;
